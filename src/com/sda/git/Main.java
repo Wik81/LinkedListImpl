e122953902738acc588ce6a914059d534215f578
Wik81 <wik99@wp.pl>
2018-10-21 13:39:22 +0200

    zmiana w main

--- src/com/sda/git/Main.java
+++ src/com/sda/git/Main.java
@@ -7,14 +7,6 @@ public class Main {
 
     public static void main(String[] args) {
 
-        List<String>lista = new LinkedList<>();
-
-        lista.add("Adam");
-        lista.add("Jola");
-        lista.add("Karol");
-
-        for(String l: lista){
-            System.out.println(l);
-        }
+        
     }
 }
